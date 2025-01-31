34f5d4ed160d89fc82f3812922cc8c3020b6b7a5
OwenHernandez <owentoto423@gmail.com>
2025-01-31 20:28:06 +0000

    feat: started the Navbar

---
+++ BaboonFeed-F/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <router-link :to="{ name: 'Home' }" class="navbar-brand">Home</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link :to="{ name: '' }" class="nav-link" :class="{ active: route.path === '/' }">Example</router-link>
+          <router-link :to="{ name: '' }" class="nav-link" :class="{ active: route.path === '/' }">Example</router-link>
+          <router-link :to="{ name: '' }" class="nav-link" :class="{ active: route.path === '/' }">Example</router-link>
+          <router-link :to="{ name: '' }" class="nav-link" :class="{ active: route.path === '/' }">Example</router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <router-view />
+</template>
+
+<script setup lang="ts">
+import { useRouter, useRoute } from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
+
+
+</script>
