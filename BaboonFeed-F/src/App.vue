961ddbb6f822ffaf839e1af6c11ff5032c02b587
Angel-L-G <angelchiquitopons@gmail.com>
2025-02-05 15:36:28 +0000

    feat: added fontawesome dependencies and plugin;

--- BaboonFeed-F/src/App.vue
+++ BaboonFeed-F/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 </script>
 
 <template>
   <div class="container">
     <h2 class="primary">
       ABC
+      <FontAwesomeIcon icon="fa-solid fa-dog" />
     </h2>
     <button class="btn btn-primary">AAAAAAa</button>
   </div>
