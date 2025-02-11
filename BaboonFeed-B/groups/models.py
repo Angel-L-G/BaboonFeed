7b392ede7dfb364f7fdfeb5c7f41797e53d16455
OwenHernandez <owentoto423@gmail.com>
2025-02-11 16:21:53 +0000

    feat: done the models for user #11

--- BaboonFeed-B/groups/models.py
+++ BaboonFeed-B/groups/models.py
@@ -1,3 +1,8 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class GroupChat(models.Model):
+    name = models.CharField(max_length=100)
+    avatar = models.ImageField(upload_to='avatars/group/', default='avatars/group/default.png')
+    leader = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='groups_leadered', on_delete=models.SET_NULL, null=True)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='member_of')
--- BaboonFeed-B/users/models.py
+++ BaboonFeed-B/users/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Follow(models.Model):
+    following = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='following', on_delete=models.CASCADE)
+    followed_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='followed_by', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['following', 'followed_by']
+
+class User(AbstractUser):
+    bio = models.TextField()
+    avatar = models.ImageField(upload_to='avatars/user', default='avatars/user/default.png')
+    follows = models.ManyToManyField('self', related_name='follows_users', symmetrical=False, through=Follow)
+    blocked = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='blocked_users')
+
+# Follow.objects.filter(following=user.pk)
+# Follow.objects.filter(followed_by=user.pk)
